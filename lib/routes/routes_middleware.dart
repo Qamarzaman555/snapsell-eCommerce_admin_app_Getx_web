903a0de799220f2c390a86993aa226979ed286f2
Qamar Zaman <qamarzk12345@gmail.com>
2024-10-20 20:49:25 +0500

    updated: middleware

--- lib/routes/routes_middleware.dart
+++ lib/routes/routes_middleware.dart
@@ -3,13 +3,24 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../routes/routes.dart';
+import '../data/repositories/authentication/authentication_repository.dart';
+
 class TRouteMiddleware extends GetMiddleware {
   @override
   RouteSettings? redirect(String? route) {
-    log('........... Middleware Called ............');
-    const isAuthenticated = true;
-    // return isAuthenticated
-    //     ? null
-    //     : const RouteSettings(name: TRoutes.firstScreen);
+    log('........... Middleware Called for $route ............');
+
+    // Check if user is authenticated
+    bool isAuthenticated = AuthenticationRepository.instance.isAuthenticated;
+
+    // Redirect based on authentication
+    if (!isAuthenticated) {
+      // If not authenticated, redirect to the login page
+      return const RouteSettings(name: TRoutes.login);
+    }
+
+    // Otherwise, proceed to the requested route
+    return null;
   }
 }
